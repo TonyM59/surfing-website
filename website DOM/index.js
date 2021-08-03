f6b8336a0959ea72a8c1d849192ef656cfd5a759
TonyM59 <tonymahlangu59@gmail.com>
2021-08-04 01:25:19 +0200

    updated j.s file

--- website DOM/index.js
+++ website DOM/index.js
@@ -7,12 +7,51 @@ const home = document.createElement('li');
 const aboutUs = document.createElement('li');
 const product = document.createElement('li');
 const contact = document.createElement('li');
+
+   //   background image
 const mainContent  = document.createElement('div');
 mainContent.classList.add('maincontent');
 const section = document.createElement('section');
 section.classList.add('background')
 const h1 = document.createElement('h1');
-/*image.classList.add('background')*/
+
+     // adding the pictures
+const contentWrapper = document.createElement('div');
+contentWrapper.classList.add('contentwrapper');
+const leftSection = document.createElement('div');
+leftSection.classList.add('leftSection');
+const rightSection = document.createElement('div');
+rightSection.classList.add('rightSection');
+const middleSection = document.createElement('div');
+middleSection.classList.add('middleSection');
+
+//adding footer
+const footer = document.createElement('footer');
+footer.classList.add('footer');
+
+// adding facebook icon
+const icons = document.createElement('p');
+icons.classList.add('icons')
+const fbLink= document.createElement('a');
+const fbook= document.createElement('i');
+fbook.className = 'fa fa-facebook-official'
+fbLink.href = "https://www.facebook.com/surfemporium"
+
+//adding twitter icon
+const twitterLink= document.createElement('a');
+const twitter= document.createElement('i');
+twitter.className = 'fa fa-twitter'
+twitterLink.href = "https://twitter.com/surfemporium"
+//adding instargram icon
+const instagramLink= document.createElement('a');
+const instagram= document.createElement('i');
+instagram.className = 'fa fa-instagram'
+instagramLink.href = "https://www.instagram.com/surfemporium/"
+
+
+
+// adding copyright
+const copyright = document.createElement('p');
 
 
 home.textContent = 'Home';
@@ -20,22 +59,44 @@ aboutUs.textContent = 'About us';
 product.textContent = 'Product';
 contact.textContent = 'Contact';
 
-h1.textContent = 'MASTER SURFING'
+h1.textContent = 'SURFING MASTERS'
 
-/*image.src = 'images/logo.jpg'*/
+copyright.textContent = 'Copyright ©️ 2021 Master Surfing.All Rights Reversed'
 
-/*document.body.style.backgroundImage="url('images/photo1.jpg');"*/
-
-
+   // append navbar to the parent banner
 menuBar.appendChild(home);
 menuBar.appendChild(aboutUs);
 menuBar.appendChild(product);
 menuBar.appendChild(contact);
 content.appendChild(menuBar);
 banner.appendChild(content);
+
+  //append main content
 mainContent.appendChild(section);
 section.appendChild(h1)
-banner.appendChild(mainContent); 
+banner.appendChild(mainContent);
+
+// append the three pictures
+contentWrapper.appendChild(rightSection);
+contentWrapper.appendChild(leftSection);
+contentWrapper.appendChild(middleSection)
+mainContent.appendChild(contentWrapper);
+
+// append icons
+icons.appendChild(fbook);
+icons.appendChild(fbLink);
+fbLink.appendChild(fbook);
+icons.appendChild(twitterLink);
+icons.appendChild(twitter);
+twitterLink.appendChild(twitter);
+icons.appendChild(instagramLink);
+icons.appendChild(instagram);
+instagramLink.appendChild(instagram);
+footer.appendChild(icons);
+footer.appendChild(copyright);
+banner.appendChild(footer);
+
+
 
 return{banner}
 
